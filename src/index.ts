80b4494ade0f91d1eb2049d5191987450713dc6d
Elí Maciel <tecno_eli@hotmail.com>
2025-09-13 18:38:14 -0600

    process models

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
-import pgh from '@prisma/generator-helper'
+import pgh, { DMMF } from '@prisma/generator-helper'
 import { writeFile } from 'node:fs/promises'
 import ds from './default-schema.json' with { type: 'json' }
 import type { ErdEditorSchema, MemoEntity, TableEntity } from './types.ts'
 import { genId } from './id.ts'
 
 const defaultSchema = ds as ErdEditorSchema
+
+let zIndex = 2
+let x = 0
+let y = 0
+const enumList = new Map<string, MemoEntity>()
+const modelList = new Map<string, TableEntity>()
+
 pgh.generatorHandler({
     onManifest: gc => {
         return {
@@ -16,58 +23,9 @@ pgh.generatorHandler({
     onGenerate: async ({ generator, dmmf }) => {
         const { datamodel, mappings, schema } = dmmf
         const { enums, models, ...datamodelRest } = datamodel
-        const enumList = new Map<string, MemoEntity>()
-        let zIndex = 2
-        let x = 0
-        let y = 0
-        // console.log('datamodel', datamodelRest)
-        for (const enum_ of enums) {
-            const id = genId()
-            enumList.set(id, {
-                id,
-                ui: {
-                    x: x * 100,
-                    y: y,
-                    zIndex: zIndex++,
-                    width: 100,
-                    height: 100,
-                    color: '',
-                },
-                meta: {
-                    updateAt: Date.now(),
-                    createAt: Date.now(),
-                },
-                value: `${enum_.dbName ?? enum_.name}\n\n${enum_.values.map(v => v.dbName ?? v.name).join('\n')}`,
-            })
-        }
-        defaultSchema.collections.memoEntities = Object.fromEntries(enumList)
-        defaultSchema.doc.memoIds = Array.from(enumList.keys())
 
-        // const modelList = new Map<string, TableEntity>()
-        // for (const model of models) {
-        //     console.log(model)
-        //     const id = genId()
-        //     modelList.set(id, {
-        //         "id": "r9NohBARMmqUpopnzzXlU",
-        //         "name": "TableName",
-        //         "comment": "",
-        //         "columnIds": [],
-        //         "seqColumnIds": [],
-        //         "ui": {
-        //           "x": 211,
-        //           "y": 347,
-        //           "zIndex": 4,
-        //           "widthName": 61,
-        //           "widthComment": 60,
-        //           "color": ""
-        //         },
-        //         "meta": {
-        //           "updateAt": 1757808898478,
-        //           "createAt": 1757808890863
-        //         }
-        //       })
-        //     break
-        // }
+        processEnums(enums)
+        processModels(models)
 
         writeFile(
             generator.output!.value as string,
@@ -75,3 +33,58 @@ pgh.generatorHandler({
         )
     },
 })
+
+function processEnums(enums: readonly DMMF.DatamodelEnum[]) {
+    for (const enum_ of enums) {
+        const id = genId()
+        enumList.set(id, {
+            id,
+            ui: {
+                x: x++ * 100,
+                y: y,
+                zIndex: zIndex++,
+                width: 100,
+                height: 100,
+                color: '',
+            },
+            meta: {
+                updateAt: Date.now(),
+                createAt: Date.now(),
+            },
+            value: `${enum_.dbName ?? enum_.name}\n\n${enum_.values.map(v => v.dbName ?? v.name).join('\n')}`,
+        })
+    }
+
+    defaultSchema.collections.memoEntities = Object.fromEntries(enumList)
+    defaultSchema.doc.memoIds = Array.from(enumList.keys())
+}
+
+function processModels(models: readonly DMMF.Model[]) {
+    y++
+    for (const model of models) {
+        console.log(model)
+        const id = genId()
+        modelList.set(id, {
+            id: id,
+            name: model.dbName ?? model.name,
+            comment: model.documentation ?? '',
+            columnIds: [],
+            seqColumnIds: [],
+            ui: {
+                x: 0,
+                y: y++ * 100,
+                zIndex: 4,
+                widthName: 60,
+                widthComment: 60,
+                color: '',
+            },
+            meta: {
+                updateAt: Date.now(),
+                createAt: Date.now(),
+            },
+        })
+    }
+
+    defaultSchema.collections.tableEntities = Object.fromEntries(modelList)
+    defaultSchema.doc.tableIds = Array.from(modelList.keys())
+}
